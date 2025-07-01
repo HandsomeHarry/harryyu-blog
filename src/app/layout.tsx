59c27b018b6495a29f218a3b654cf27256ab0678
HandsomeHarry <harryyu2002@gmail.com>
2025-07-01 23:27:38 +0800

    add vercel stuff

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: 'Harry Yu - Personal Blog',
@@ -25,6 +27,8 @@ export default function RootLayout({
           </main>
           <Footer />
         </div>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
